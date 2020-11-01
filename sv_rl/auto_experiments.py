a353e3e674433992b4963d788d7df6713b641ae1
Jesse Zhang <jessez@usc.edu>
2020-11-01 15:13:06 -0800

    updated with svt and auto_experiments.py

---
+++ sv_rl/auto_experiments.py
@@ -0,0 +1,84 @@
+import random
+import os
+from absl import logging, flags, app
+from multiprocessing import Queue, Manager
+from pathos import multiprocessing
+import traceback
+import time
+which_gpus = [0, 1, 2, 3]
+#which_gpus = [1]
+max_worker_num = len(which_gpus) * 2
+COMMANDS = [
+    "python3 main.py " 
+]
+batch_sizes = [16, 32, 64]
+maskps = [
+    #0.1,
+    0.5,
+    0.9
+]
+me_types = ["svt","nucnorm", "softimp"]
+envs = ["BreakoutNoFrameskip-v4", "FrostbiteNoFrameskip-v4"]
+svrls = ["--svrl"] #""]
+env_name_map = {
+    "BreakoutNoFrameskip-v4":"breakout", 
+    "FrostbiteNoFrameskip-v4":"frostbite",
+}
+timestamp_map = {
+    "BreakoutNoFrameskip-v4":15000000, 
+    "FrostbiteNoFrameskip-v4":10000000,
+}
+def _init_device_queue(max_worker_num):
+    m = Manager()
+    device_queue = m.Queue()
+    for i in range(max_worker_num):
+        idx = i % len(which_gpus)
+        gpu = which_gpus[idx]
+        device_queue.put(gpu)
+    return device_queue
+
+def run():
+    """Run trainings with all possible parameter combinations in
+    the configured space.
+    """
+
+    process_pool = multiprocessing.Pool(
+        processes=max_worker_num, maxtasksperchild=1)
+    device_queue = _init_device_queue(max_worker_num)
+
+    for command in COMMANDS:
+        for env in envs:
+            for me_type in me_types:
+                for maskp in maskps:
+                    for batch_size in batch_sizes:
+                        for svrl in svrls:
+                            if svrl == "--svrl":
+                                svrl_string = "svrl"
+                            else:
+                                svrl_string = ""
+                            command += f'--env {env} {svrl} --me_type {me_type} --maskp {maskp} --batch-size {batch_size} --num_timesteps {timestamp_map[env]} {env_name_map[env]}_dqn_{svrl_string}_{me_type}' 
+                            process_pool.apply_async(
+                                func=_worker,
+                                args=[command, device_queue],
+                                error_callback=lambda e: logging.error(e))
+                            process_pool.close()
+                            process_pool.join()
+
+def _worker(command, device_queue):
+    # sleep for random seconds to avoid crowded launching
+    try:
+        time.sleep(random.uniform(0, 3))
+
+        device = device_queue.get()
+
+        logging.set_verbosity(logging.INFO)
+
+        logging.info("command %s" % command)
+        os.system("CUDA_VISIBLE_DEVICES=%d " % device + command)
+
+        device_queue.put(device)
+    except Exception as e:
+        logging.info(traceback.format_exc())
+        raise e
+run()
+
--- sv_rl/utils/svrl_utils.py
+++ sv_rl/utils/svrl_utils.py
@@ -4,6 +4,7 @@ import math, time
 
 import cvxpy as cvx
 from fancyimpute import SoftImpute, BiScaler
+from matrix_completion import svt_solve
 
 
 def softimp(inputs, h, w, mask_prob):
@@ -21,6 +22,18 @@ def softimp(inputs, h, w, mask_prob):
     W[W > 1] = 1
     est_matrix = W * divide_val + subtract_val
 
+    return torch.from_numpy(est_matrix)
+
+def svt(inputs, h, w, mask_prob):
+    inputs = inputs.cpu().numpy()
+    subtract_val = (inputs.max() + inputs.min()) / 2.
+    divide_val = (inputs.max() - inputs.min()) / 2. + 1e-7
+    inputs = (inputs - subtract_val) / divide_val
+    mask = np.random.binomial(1, mask_prob, h * w).reshape(h, w).astype(float)
+    W = svt_solve(inputs, mask, epsilon=1e-3, max_iterations=100)
+    W[W < -1] = -1
+    W[W > 1] = 1
+    est_matrix = W * divide_val + subtract_val
     return torch.from_numpy(est_matrix)
 
 
